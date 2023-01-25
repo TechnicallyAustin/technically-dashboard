5708d0bb24462d16650508bf2450a86ff90958ac
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-01-25 01:09:51 -0700

    side bar work in progress, added divs for each icon and menu item in the side bar

--- src/api.js
+++ src/api.js
@@ -32,28 +32,144 @@ function dashBoard(){
             function home(){
                 const home = menu.appendChild(document.createElement("div"));
                 home.setAttribute("class", "menu-item home-container");
+
+                function homeIcon(){
+                    const iconContainer = home.appendChild(document.createElement("div"))
+                    const icon = iconContainer.appendChild(document.createElement("img"))
+                    icon.setAttribute("src", "")
+                    icon.setAttribute("alt", "menu-logo")
+                    icon.setAttribute("class", "menu-icon")
+                }; homeIcon()
+
+                function homeText(){
+                    const textContainer = home.appendChild(document.createElement("div"));
+                    textContainer.setAttribute("class", "text-container");
+
+                    const text = textContainer.appendChild(document.createElement("h3"));
+                    text.setAttribute("class"," menu-text")
+                    text.textContent = "Home"
+                }; homeText()
+
             }; home(
 
             )
             function profile(){
                 const profile = menu.appendChild(document.createElement("div"));
                 profile.setAttribute("class", "menu-item profile-container");
+
+                function profileIcon(){
+                    const iconContainer = profile.appendChild(document.createElement("div"))
+                    const icon = iconContainer.appendChild(document.createElement("img"))
+                    icon.setAttribute("src", "")
+                    icon.setAttribute("alt", "menu-logo")
+                    icon.setAttribute("class", "menu-icon")
+                }; profileIcon()
+
+                function profileText(){
+                    const textContainer = profile.appendChild(document.createElement("div"));
+                    textContainer.setAttribute("class", "text-container");
+
+                    const text = textContainer.appendChild(document.createElement("h3"));
+                    text.setAttribute("class"," menu-text")
+                    text.textContent = "Profile"
+
+                }; profileText()
+
+
             }; profile()
 
             function messages(){
                 const messages = menu.appendChild(document.createElement("div"));
                 messages.setAttribute("class", "menu-item messages-container");
+
+                function messagesIcon(){
+                    const iconContainer = messages.appendChild(document.createElement("div"))
+                    const icon = iconContainer.appendChild(document.createElement("img"))
+                    icon.setAttribute("src", "")
+                    icon.setAttribute("alt", "menu-logo")
+                    icon.setAttribute("class", "menu-icon")
+                }; messagesIcon()
+
+                function messagesText(){
+                    const textContainer = messages.appendChild(document.createElement("div"));
+                    textContainer.setAttribute("class", "text-container");
+
+                    const text = textContainer.appendChild(document.createElement("h3"));
+                    text.setAttribute("class"," menu-text")
+                    text.textContent = "Messages"
+                }; messagesText()
+
             }; messages()
 
             function history(){
                 const history = menu.appendChild(document.createElement("div"));
                 history.setAttribute("class", "menu-item history-container");
+
+                function historyIcon(){
+                    const iconContainer = history.appendChild(document.createElement("div"))
+                    const icon = iconContainer.appendChild(document.createElement("img"))
+                    icon.setAttribute("src", "")
+                    icon.setAttribute("alt", "menu-logo")
+                    icon.setAttribute("class", "menu-icon")
+                }; historyIcon()
+
+                function historyText(){
+                    const textContainer = history.appendChild(document.createElement("div"));
+                    textContainer.setAttribute("class", "text-container");
+
+                    const text = textContainer.appendChild(document.createElement("h3"));
+                    text.setAttribute("class"," menu-text")
+                    text.textContent = "History"
+                }; historyText()
+
             }; history()
 
             function tasks(){
                 const tasks = menu.appendChild(document.createElement("div"));
                 tasks.setAttribute("class", "menu-item tasks-container");
+
+                function tasksIcon(){
+                    const iconContainer = tasks.appendChild(document.createElement("div"))
+                    const icon = iconContainer.appendChild(document.createElement("img"))
+                    icon.setAttribute("src", "")
+                    icon.setAttribute("alt", "menu-logo")
+                    icon.setAttribute("class", "menu-icon")
+                }; tasksIcon()
+
+                function tasksText(){
+
+                    const textContainer = tasks.appendChild(document.createElement("div"));
+                    textContainer.setAttribute("class", "text-container");
+
+                    const text = textContainer.appendChild(document.createElement("h3"));
+                    text.setAttribute("class"," menu-text")
+                    text.textContent = "Tasks"
+                }; tasksText()
+
             }; tasks()
+
+              function communities() {
+                const community = menu.appendChild(document.createElement("div"));
+                community.setAttribute("class", "menu-item community-container")
+                
+                function communityIcon(){
+                    const iconContainer = community.appendChild(document.createElement("div"))
+                    const icon = iconContainer.appendChild(document.createElement("img"))
+                    icon.setAttribute("src", "")
+                    icon.setAttribute("alt", "menu-logo")
+                    icon.setAttribute("class", "menu-icon")
+                }; communityIcon()
+
+                function communityText(){
+                    const textContainer = community.appendChild(document.createElement("div"));
+                    textContainer.setAttribute("class", "text-container");
+
+                    const text = textContainer.appendChild(document.createElement("h3"));
+                    text.setAttribute("class", "menu-text");
+                    text.textContent = "Communities"
+                }; communityText()
+              }; communities()
+
         }; menu()
 
         function options(){
@@ -63,16 +179,72 @@ function dashBoard(){
             function settings(){
                 const settings = options.appendChild(document.createElement("div"));
                 settings.setAttribute("class", "option-item settings-container")
+
+                 function settingIcon(){
+                    const iconContainer = settings.appendChild(document.createElement("div"))
+                    const icon = iconContainer.appendChild(document.createElement("img"))
+                    icon.setAttribute("src", "")
+                    icon.setAttribute("alt", "menu-logo")
+                    icon.setAttribute("class", "menu-icon")
+                }; settingIcon()
+
+                function settingText(){
+
+                    const textContainer = settings.appendChild(document.createElement("div"));
+                    textContainer.setAttribute("class", "text-container");
+
+                    const text = textContainer.appendChild(document.createElement("h3"));
+                    text.setAttribute("class", "menu-text");
+                    text.textContent = "Settings"
+                }; settingText()
+
+
             }; settings()
 
             function support(){
                 const support = options.appendChild(document.createElement("div"));
                 support.setAttribute("class", "option-item  support-container");
+
+                 function supportIcon(){
+                    const iconContainer = support.appendChild(document.createElement("div"))
+                    const icon = iconContainer.appendChild(document.createElement("img"))
+                    icon.setAttribute("src", "")
+                    icon.setAttribute("alt", "menu-logo")
+                    icon.setAttribute("class", "menu-icon")
+                }; supportIcon()
+
+                function supportText(){
+                    const textContainer = support.appendChild(document.createElement("div"));
+                    textContainer.setAttribute("class", "text-container");
+
+                    const text = textContainer.appendChild(document.createElement("h3"));
+                    text.setAttribute("class", "menu-text");
+                    text.textContent = "Support"
+                }; supportText()
+
             }; support()
 
             function privacy(){
                 const privacy = options.appendChild(document.createElement("div"));
-                privacy.setAttribute("class", "option-item  privacy-container");                
+                privacy.setAttribute("class", "option-item  privacy-container");
+                
+                 function privacyIcon(){
+                    const iconContainer = privacy.appendChild(document.createElement("div"))
+                    const icon = iconContainer.appendChild(document.createElement("img"))
+                    icon.setAttribute("src", "")
+                    icon.setAttribute("alt", "menu-logo")
+                    icon.setAttribute("class", "menu-icon")
+                }; privacyIcon()
+
+                function privacyText(){
+                    const textContainer = privacy.appendChild(document.createElement("div"));
+                    textContainer.setAttribute("class", "text-container");
+
+                    const text = textContainer.appendChild(document.createElement("h3"));
+                    text.setAttribute("class", "menu-text");
+                    text.textContent = "Privacy"
+                }; privacyText()
+
             }; privacy()
         }; options()
 
