bfcc2e764d447d86ad1ffb6028d7059cbac8960f
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-01-24 21:36:04 -0700

    set general structure

---
+++ src/api.js
@@ -0,0 +1,58 @@
+function dashBoard(){
+    function sidebar(){
+        function logo(){
+            function logoIcon(){};
+            function logoText(){};
+        };
+          
+        function menu(){
+            function home(){};
+            function profile(){};
+            function messages(){}
+            function history(){};
+            function tasks(){};
+        };
+
+        function options(){
+            function settings(){};
+            function support(){};
+            function privacy(){};
+        };
+    };
+
+    function header(){
+        function searchSection(){
+            function searchIcon(){};
+            function searchBar(){};
+        };
+
+        function profileSettings(){
+            function notifications(){};
+            function profileIcon(){};
+            function profileName(){};
+        };
+
+        function welcome(){
+            function welcomeIcon(){};
+            function welcomeMessage(){};
+        };
+
+        function buttons(){
+            function newButton(){};
+            function uploadButton(){};
+            function shareButton(){};
+        };
+    };
+
+    function main(){
+        function projects(){};
+        function announcements(){
+            function announce(){};
+            function trending(){};
+        };
+    };
+
+    
+
+
+};
